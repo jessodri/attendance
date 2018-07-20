d4f73d2009907c980a592d01dfee359c3e448745
Andrew Whalley <andrewcwhalley@gmail.com>
2018-07-20 11:22:01 +1000

    Mup init & settings for deployment to digital ocean droplet

---
+++ meteor-app/.deploy/mup.js
@@ -0,0 +1,57 @@
+module.exports = {
+  servers: {
+    one: {
+      // TODO: set host address, username, and authentication method
+      host: '159.65.1.63',
+      username: 'root',
+      // pem: '~/.ssh/id_rsa'
+      // password: 'server-password'
+      // or neither for authenticate from ssh-agent
+    }
+  },
+
+  app: {
+    // TODO: change app name and path
+    name: 'bike-assessment',
+    path: '../',
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+    
+    env: {
+      // TODO: Change to your app's url
+      // If you are using ssl, it needs to start with https://
+      ROOT_URL: 'http://bike.assessment.whalley.com',
+      MONGO_URL: 'mongodb+srv://eryk:Hxft2q4y2@bikeassessment-qd9hs.mongodb.net/assessment'
+    },
+    
+    docker: {
+      // change to 'abernix/meteord:base' if your app is using Meteor 1.4 - 1.5
+      image: 'abernix/meteord:node-8.4.0-base',
+    },
+    
+    // Show progress bar while uploading bundle to server
+    // You might need to disable it on CI servers
+    enableUploadProgressBar: true,
+    deployCheckWaitTime: 600,
+    enableUploadProgressBar: true
+  },
+
+  // (Optional)
+  // Use the proxy to setup ssl or to route requests to the correct
+  // app when there are several apps
+
+  // proxy: {
+  //   domains: 'mywebsite.com,www.mywebsite.com',
+
+  //   ssl: {
+  //     // Enable Let's Encrypt
+  //     letsEncryptEmail: 'email@domain.com'
+  //   }
+  // }
+};
